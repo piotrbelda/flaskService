bdf5e3852eefaeb0331b0e0e5129cb4e42d101ac
piotrek <piotrek.belda@wp.pl>
2021-02-08 22:07:05 +0100

    Reading into DB

--- project/data/bin/basic_inserts.py
+++ project/data/bin/basic_inserts.py
@@ -1,5 +1,8 @@
 import os
 import data.db_session as db_session
+from data.package import Package
+from sqlalchemy.orm import Session
+from data.releases import Release
 
 def init_db():
     top_folder = os.path.dirname(__file__)
@@ -7,8 +10,39 @@ def init_db():
     db_file = os.path.abspath(os.path.join(top_folder, rel_file))
     db_session.global_init(db_file)
 
+def insert_a_package():
+
+    p = Package()
+    p.id = input("Package id / name: ").strip().lower()
+    p.summary = input("Package summary: ").strip()
+    p.author_name = input("Author: ").strip()
+    p.license = input("License: ").strip()
+    p.description = input("Description: ").strip()
+
+    print("Release 1:")
+    r = Release()
+    r.major_ver = int(input("Major version: "))
+    r.minor_ver = int(input("Minor version: "))
+    r.build_ver = int(input("Build version: "))
+    r.size = int(input("Size: "))
+    p.releases.append(r)
+
+    print("Release 2:")
+    r = Release()
+    r.major_ver = int(input("Major version: "))
+    r.minor_ver = int(input("Minor version: "))
+    r.build_ver = int(input("Build version: "))
+    r.size = int(input("Size: "))
+    p.releases.append(r)
+
+    session: Session = db_session.create_session()
+    session.add(p)
+    session.commit()
+
 def main():
     init_db()
+    while True:
+        insert_a_package()
 
 if __name__ == "__main__":
     main()
--- project/data/db_session.py
+++ project/data/db_session.py
@@ -3,17 +3,21 @@ import sqlalchemy.orm as orm
 
 from data.modelbase import SqlAlchemyBase
 
-factory = None
+__factory = None
 
 def global_init(db_file: str):
-    global factory
-    if factory:
+    global __factory
+    if __factory:
         return
     if not db_file or not db_file.strip():
         raise Exception("You must specify a db file.")
     conn_str = "sqlite:///" + db_file.strip()
     engine = create_engine(conn_str, echo=False)
-    factory = orm.sessionmaker(bind=engine)
+    __factory = orm.sessionmaker(bind=engine)
 
     import data.__all_models
     SqlAlchemyBase.metadata.create_all(engine)
+
+def create_session() -> orm.Session:
+    global __factory
+    return __factory()
